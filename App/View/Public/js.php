75f6aba84f11e171835068ac65a678b2ffa5a6f4
enjoyLift <YOUR EMAIL ADDRESS383550559@qq.com>
2016-08-31 20:45:00 +0800

    layer插件引入

--- App/View/Public/js.php
+++ App/View/Public/js.php
@@ -3,4 +3,5 @@
  	<script src="./Js/vue-resource.min.js"></script>
  	<script src="./Js/vue-router.min.js"></script>
  	<script src="./Js/bootstrap.min.js"></script>
+ 	<script src="./Js/layer.js"></script>
  	<script src="./Js/host.js"></script>
