7f1a502ba238ea336bdd0905fb1a522bbc1dd022
zhangguangqiang <zhangguangqiang@weiba66.com>
2016-08-31 20:44:35 +0800

    222222222

--- SinglePHP.class.php
+++ SinglePHP.class.php
@@ -281,7 +281,6 @@ class SinglePHP {
 
         //检测如果是微信客户端,让用户授权
         if(($this->a != "getcode") && strpos($_SERVER["HTTP_USER_AGENT"],"MicroMessenger")){
-            dump('微信');die;
             $jssdk->getCode();
         }
 
