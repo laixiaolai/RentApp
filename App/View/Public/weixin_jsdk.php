dbb7d8bd2d0b0ddef4f21aef393d0c5896461f4c
zhangguangqiang <zhangguangqiang@weiba66.com>
2016-08-31 19:52:20 +0800

    2222222

--- App/View/Public/weixin_jsdk.php
+++ App/View/Public/weixin_jsdk.php
@@ -67,7 +67,7 @@
 	    ]
 	  });
 	  wx.ready(function () {
-	    // 在这里调用 API
+	    // 分享好友
 	    wx.onMenuShareAppMessage({
 	        title: _title, // 分享标题
 	        desc: _desc, // 分享描述
@@ -76,7 +76,65 @@
 	        type: '', // 分享类型,music、video或link，不填默认为link
 	        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
 	        success: function () { 
-	            alert('分享成功');
+	            //alert('分享成功');
+	        },
+	        cancel: function () { 
+	            // 用户取消分享后执行的回调函数
+	        }
+	    });
+
+	    //分享朋友圈
+	    wx.onMenuShareAppMessage({
+	        title: _title, // 分享标题
+	        desc: _desc, // 分享描述
+	        link: _host, // 分享链接
+	        imgUrl: _imgUrl, // 分享图标
+	        type: '', // 分享类型,music、video或link，不填默认为link
+	        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+	        success: function () { 
+	            // 用户确认分享后执行的回调函数
+	        },
+	        cancel: function () { 
+	            // 用户取消分享后执行的回调函数
+	        }
+	    });
+
+	    //分享qq
+	    wx.onMenuShareQQ({
+	        title: _title, // 分享标题
+	        desc: _desc, // 分享描述
+	        link: _host, // 分享链接
+	        imgUrl: _imgUrl, // 分享图标
+	        success: function () { 
+	           // 用户确认分享后执行的回调函数
+	        },
+	        cancel: function () { 
+	           // 用户取消分享后执行的回调函数
+	        }
+	    });
+
+	    //分享微博
+	    wx.onMenuShareWeibo({
+	        title: _title, // 分享标题
+	        desc: _desc, // 分享描述
+	        link: _host, // 分享链接
+	        imgUrl: _imgUrl, // 分享图标
+	        success: function () { 
+	           // 用户确认分享后执行的回调函数
+	        },
+	        cancel: function () { 
+	            // 用户取消分享后执行的回调函数
+	        }
+	    });
+
+	    //分享qq空间
+	    wx.onMenuShareQZone({
+	        title: _title, // 分享标题
+	        desc: _desc, // 分享描述
+	        link: _host, // 分享链接
+	        imgUrl: _imgUrl, // 分享图标
+	        success: function () { 
+	           // 用户确认分享后执行的回调函数
 	        },
 	        cancel: function () { 
 	            // 用户取消分享后执行的回调函数
