75567440f9c989f49e5a8745ec7fdc5dde39946f
zhangguangqiang <zhangguangqiang@weiba66.com>
2016-09-02 10:48:16 +0800

    1111111111111

--- App/Controller/IndexController.class.php
+++ App/Controller/IndexController.class.php
@@ -73,7 +73,59 @@ class IndexController extends BaseController {
         $this->assign('title', '产品预定页面');
         $this->display();
     }
+
+    //http://html5.3ddysj.com/index.php?a=Jsapi
+    public function JsapiAction(){  
+        dump("Jsapi");
+    }
+
     public function BuyAction(){
+        ini_set('date.timezone','Asia/Shanghai');
+        //error_reporting(E_ERROR);
+        require_once ROOT_PATH."/Lib/weixin/WxPay.Api.php";
+        require_once ROOT_PATH."/Lib/weixin/WxPay.JsApiPay.php";
+        require_once ROOT_PATH."/Lib/weixin/WxLog.php";
+
+
+
+        //初始化日志
+        $logHandler= new CLogFileHandler(ROOT_PATH."/Log/weixin_".date('Y-m-d').'.log');
+        $log = WxLog::Init($logHandler, 15);
+
+        //①、获取用户openid
+        $tools = new JsApiPay();
+        $openId = $tools->GetOpenid();
+
+        //②、统一下单
+        $input = new WxPayUnifiedOrder();
+        $input->SetBody("test");
+        $input->SetAttach("test");
+        $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+        $input->SetTotal_fee("1");
+        $input->SetTime_start(date("YmdHis"));
+        $input->SetTime_expire(date("YmdHis", time() + 600));
+        $input->SetGoods_tag("test");
+        $input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
+        $input->SetTrade_type("JSAPI");
+        $input->SetOpenid($openId);
+        $order = WxPayApi::unifiedOrder($input);
+        echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
+        $this->printf_info($order);
+        $jsApiParameters = $tools->GetJsApiParameters($order);
+
+        //获取共享收货地址js函数参数
+        $editAddress = $tools->GetEditAddressParameters();
+
+        //③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
+        /**
+         * 注意：
+         * 1、当你的回调地址不可访问的时候，回调通知会失败，可以通过查询订单来确认支付是否成功
+         * 2、jsapi支付时需要填入用户openid，WxPay.JsApiPay.php中有获取openid流程 （文档可以参考微信公众平台“网页授权接口”，
+         * 参考http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html）
+         */
+
+
+
         $this->assign('title', '支付页面');
         $this->display();
     }
@@ -125,5 +177,14 @@ class IndexController extends BaseController {
         Log::debug('something');
         Log::sql('something');
         echo '请到Log文件夹查看效果。如果是SAE环境，可以在日志中心的DEBUG日志查看。';
+    }
+
+
+    //打印输出数组信息
+    public function printf_info($data)
+    {
+        foreach($data as $key=>$value){
+            echo "<font color='#00ff55;'>$key</font> : $value <br/>";
+        }
     }
 }
--- App/View/Index/Buy.php
+++ App/View/Index/Buy.php
@@ -1,29 +1,75 @@
-<?php
-$data = array(
-	'title'      => $title,
-	'body_class' => 'bs-docs-home',
-	'css'        => array(),
-	'js'         => array("./Js/jquery.min.js","./Js/vue.min.js","./Js/vue-resource.min.js","./Js/vue-router.min.js"),
-);
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+    <title>微信支付样例-支付</title>
+    <script type="text/javascript">
+	//调用微信JS api 支付
+	function jsApiCall()
+	{
+		WeixinJSBridge.invoke(
+			'getBrandWCPayRequest',
+			<?php echo $jsApiParameters; ?>,
+			function(res){
+				WeixinJSBridge.log(res.err_msg);
+				alert(res.err_code+res.err_desc+res.err_msg);
+			}
+		);
+	}
 
-//载入头部
-View::tplInclude('Public/header', $data); 
-
-?>
-
-
-<a href="./index.php">首页</a><br/>
-<a href="./index.php?a=list">产品列表页</a><br/>
-<a href="./index.php?a=info">产品详情页</a><br/>
-<a href="./index.php?a=pin">添加评论页</a><br/>
-<a href="./index.php?a=yudin">产品预定页面</a><br/>
-<a href="./index.php?a=buy">支付页面</a><br/>
-<a href="./index.php?a=buyok">支付成功页面</a><br/>
-<a href="./index.php?a=test">测试列表数据</a><br/>
-
-<h1>这是<?php echo $title;?></h1>
-
-<?php 
-//载入底部
-View::tplInclude('Public/footer'); 
-?>
+	function callpay()
+	{
+		if (typeof WeixinJSBridge == "undefined"){
+		    if( document.addEventListener ){
+		        document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+		    }else if (document.attachEvent){
+		        document.attachEvent('WeixinJSBridgeReady', jsApiCall); 
+		        document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+		    }
+		}else{
+		    jsApiCall();
+		}
+	}
+	</script>
+	<script type="text/javascript">
+	//获取共享地址
+	function editAddress()
+	{
+		WeixinJSBridge.invoke(
+			'editAddress',
+			<?php echo $editAddress; ?>,
+			function(res){
+				var value1 = res.proviceFirstStageName;
+				var value2 = res.addressCitySecondStageName;
+				var value3 = res.addressCountiesThirdStageName;
+				var value4 = res.addressDetailInfo;
+				var tel = res.telNumber;
+				
+				alert(value1 + value2 + value3 + value4 + ":" + tel);
+			}
+		);
+	}
+	
+	window.onload = function(){
+		if (typeof WeixinJSBridge == "undefined"){
+		    if( document.addEventListener ){
+		        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+		    }else if (document.attachEvent){
+		        document.attachEvent('WeixinJSBridgeReady', editAddress); 
+		        document.attachEvent('onWeixinJSBridgeReady', editAddress);
+		    }
+		}else{
+			editAddress();
+		}
+	};
+	
+	</script>
+</head>
+<body>
+    <br/>
+    <font color="#9ACD32"><b>该笔订单支付金额为<span style="color:#f00;font-size:50px">1分</span>钱</b></font><br/><br/>
+	<div align="center">
+		<button style="width:210px; height:50px; border-radius: 15px;background-color:#FE6714; border:0px #FE6714 solid; cursor: pointer;  color:white;  font-size:16px;" type="button" onclick="callpay()" >立即支付</button>
+	</div>
+</body>
+</html>
