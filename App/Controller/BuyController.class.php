3c439c1bbfbc29172a44f58fea3c55fca0ca9b09
张光强 <1641844790@qq.com>
2016-09-03 10:52:35 +0800

    11111

--- App/Controller/BuyController.class.php
+++ App/Controller/BuyController.class.php
@@ -63,6 +63,32 @@ class BuyController extends BaseController {
         ini_set('date.timezone','Asia/Shanghai');
         //error_reporting(E_ERROR);
 
+                
+                
+                // $xmlObj=simplexml_load_string($GLOBALS); //解析回调数据
+                // $xmlObj=simplexml_load_string($GLOBALS['HTTP_RAW_POST_DATA']); //解析回调数据
+                // $appid=$xmlObj->appid;//微信appid
+                // $mch_id=$xmlObj->mch_id;  //商户号
+                // $nonce_str=$xmlObj->nonce_str;//随机字符串
+                // $sign=$xmlObj->sign;//签名
+                // $result_code=$xmlObj->result_code;//业务结果
+                // $openid=$xmlObj->openid;//用户标识
+                // $is_subscribe=$xmlObj->is_subscribe;//是否关注公众帐号
+                // $trace_type=$xmlObj->trade_type;//交易类型，JSAPI,NATIVE,APP
+                // $bank_type=$xmlObj->bank_type;//付款银行，银行类型采用字符串类型的银行标识。
+                // $total_fee=$xmlObj->total_fee;//订单总金额，单位为分
+                // $fee_type=$xmlObj->fee_type;//货币类型，符合ISO4217的标准三位字母代码，默认为人民币：CNY。
+                // $transaction_id=$xmlObj->transaction_id;//微信支付订单号
+                // $out_trade_no=$xmlObj->out_trade_no;//商户订单号
+                // $attach=$xmlObj->attach;//商家数据包，原样返回
+                // $time_end=$xmlObj->time_end;//支付完成时间
+                // $cash_fee=$xmlObj->cash_fee;
+                // $return_code=$xmlObj->return_code;
+
+                phpLog($GLOBALS);
+                echo "SUCCESS";
+
+        die;
 
 
         //error_reporting(E_ERROR);
@@ -76,9 +102,10 @@ class BuyController extends BaseController {
 
         WxLog::DEBUG("1");
         $xml = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA']: '';
+        dump($GLOBALS);
         //$notify = new WxPayNotify();
         //$rest = $notify->Handle(false);
-        WxLog::DEBUG($xml);
+        WxLog::DEBUG(dump($xml));
         WxLog::DEBUG('9');
 
 
