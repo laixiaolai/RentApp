0c20db2855e61f137cbbdf0d5b4a1503b8e7176a
张光强 <1641844790@qq.com>
2016-09-03 11:04:24 +0800

    111

--- App/Controller/IndexController.class.php
+++ App/Controller/IndexController.class.php
@@ -169,7 +169,7 @@ class IndexController extends BaseController {
         $input->SetTime_start(date("YmdHis"));
         $input->SetTime_expire(date("YmdHis", time() + 600));
         $input->SetGoods_tag("test");
-        $input->SetNotify_url("http://".$_SERVER['HTTP_HOST']."notify");
+        $input->SetNotify_url("http://".$_SERVER['HTTP_HOST']."/notify");
         $input->SetTrade_type("JSAPI");
         $input->SetOpenid($openId);
         $order = WxPayApi::unifiedOrder($input);
@@ -221,7 +221,7 @@ class IndexController extends BaseController {
         // dump($editAddress);
         // die;
 
-        $this->assign('notify_url', "http://".$_SERVER['HTTP_HOST']."notify");
+        $this->assign('notify_url', "http://".$_SERVER['HTTP_HOST']."/notify");
         $this->assign('order', $order);
         $this->assign('jsApiParameters', $jsApiParameters);
         $this->assign('editAddress', $editAddress);
