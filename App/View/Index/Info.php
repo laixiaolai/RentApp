7bf46c7baa085d946fa5a6e2a83c12b719dfc888
zhangguangqiang <zhangguangqiang@weiba66.com>
2016-09-06 10:59:04 +0800

    11111111111

--- App/View/Index/Info.php
+++ App/View/Index/Info.php
@@ -121,11 +121,16 @@
                                     <div class="Select Select--single has-value" >
                                         <div style="font-size:16px;" >
                                             <select name="number2" id="numberSmall" class="form-control filter-option">
-                                                <option>1</option>
+                                                <option selected="selected">1</option>
                                                 <option>2</option>
-                                                <option selected="selected">5</option>
                                                 <option>3</option>
                                                 <option>4</option>
+                                                <option>5</option>
+                                                <option>6</option>
+                                                <option>7</option>
+                                                <option>8</option>
+                                                <option>9</option>
+                                                <option>10</option>
                                             </select>
                                         </div>
                                     </div>
