db234c3205b35bce4a4d26ce059926935af4ef23
zhangguangqiang <zhangguangqiang@weiba66.com>
2016-09-08 17:23:09 +0800

    11111111111

--- App/View/Index/Buy.php
+++ App/View/Index/Buy.php
@@ -114,10 +114,10 @@
         });
 
     });
-
-
-
+	</script>
+	
 	<?php if($is_weixin){ ?>
+		<script>
 			//调用微信JS api 支付
 			function jsApiCall()
 			{
@@ -149,39 +149,10 @@
 				    jsApiCall();
 				}
 			}
-			// //获取共享地址
-			// function editAddress()
-			// {
-			// 	WeixinJSBridge.invoke(
-			// 		'editAddress',
-			// 		<?php echo $editAddress; ?>,
-			// 		function(res){
-			// 			var value1 = res.proviceFirstStageName;
-			// 			var value2 = res.addressCitySecondStageName;
-			// 			var value3 = res.addressCountiesThirdStageName;
-			// 			var value4 = res.addressDetailInfo;
-			// 			var tel = res.telNumber;
-						
-			// 			alert(value1 + value2 + value3 + value4 + ":" + tel);
-			// 		}
-			// 	);
-			// }
-			
-			// window.onload = function(){
-			// 	if (typeof WeixinJSBridge == "undefined"){
-			// 	    if( document.addEventListener ){
-			// 	        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
-			// 	    }else if (document.attachEvent){
-			// 	        document.attachEvent('WeixinJSBridgeReady', editAddress); 
-			// 	        document.attachEvent('onWeixinJSBridgeReady', editAddress);
-			// 	    }
-			// 	}else{
-			// 		editAddress();
-			// 	}
-			// };
-			
+		</script>	
 	<?php } ?>
-	</script>
+	
+	
 
 </body>
 </html>
