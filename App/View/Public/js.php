5eafbca2775143d3fb9605699ba85e991aa2bddf
enjoyLift <YOUR EMAIL ADDRESS383550559@qq.com>
2016-09-08 18:41:01 +0800

    日历宽度自适应

--- App/View/Public/js.php
+++ App/View/Public/js.php
@@ -1,5 +1,6 @@
  	<script src="./Js/jquery.min.js"></script>
- 	<script src="./Js/jquery-ui.min.js"></script>
+ 	<script src="./Js/jquery-ui.js"></script>
+ 	<!-- <script src="./Js/jquery-ui.min.js"></script> -->
  	<script src="./Js/bootstrap.min.js"></script>
  	<script src="./Js/layer.js"></script>
  	<script src="./Js/swiper-3.3.1.jquery.min.js"></script>
