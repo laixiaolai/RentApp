cb7859df3f415090e42fa6407c2532e00696077e
enjoyLift <YOUR EMAIL ADDRESS383550559@qq.com>
2016-09-12 17:29:02 +0800

    no message

--- App/View/Index/Info.php
+++ App/View/Index/Info.php
@@ -708,7 +708,7 @@
                     updateOnImagesReady : true,//当所有的内嵌图像（img标签）加载完成后Swiper会重新初始化
                     observer:true,
                     observeParents:true,
-                    // loop: true,
+                    loop: true,
                 });
             }, 1000);
             // function(){
